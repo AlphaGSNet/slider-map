45c28158e3dd18d6fb02666792979c1ccd6f2abb
AlphaGSNet <alpha.gsnet@outlook.com>
2019-06-01 01:23:00 +0800

    Remove the unused code

--- assets/js/app.js
+++ assets/js/app.js
@@ -650,10 +650,6 @@ function onDocumentMouseMove(event) {
 	}
 }
 
-function rotateGraph(angle) {
-  graphSystem.rotation.y = angle;
-}
-
 function changeData(year) {
 	currentYear = year;
 
@@ -918,30 +914,6 @@ function makeFat(){
 	scene.getObjectByName( 'A3' ).scale.z = 1;
 	scene.getObjectByName( 'A6' ).scale.z = 1;
 }
-
-$('.mdl-radio__button').click(function() {
-	var value = $(this).attr('value');
-
-	if (value == '1') {
-		rotateGraph(Math.PI);
-		makeFat();
-		cameraControls.enabled = true;
-		cameraTarget = new THREE.Vector3( 0, 30, 0 );
-	}
-	if (value == '2') {
-		rotateGraph(0);
-		makeFat();
-		cameraControls.enabled = true;
-		cameraTarget = new THREE.Vector3( 0, 10, 0 );
-	}
-	if (value == '3') {
-		rotateGraph(0);
-		cameraControls.reset();
-		cameraControls.enabled = false;
-		makeFlat();
-		cameraTarget = new THREE.Vector3( 0, 20, 0 );
-	}
-});
 
 $('#popup').hover(function() {
   $('#popup').removeClass('close');
